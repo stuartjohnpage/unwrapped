a44b120a241b9b27e44e1a90c12f9c669a55fd2b
Stuart Page <stuartjohnpage@live.com>
2023-01-17 10:01:04 -0600

    display attendee info

--- lib/unwrapped_web/controllers/event_controller.ex
+++ lib/unwrapped_web/controllers/event_controller.ex
@@ -15,6 +15,7 @@ defmodule UnwrappedWeb.EventController do
   end
 
   def create(conn, %{"event" => event_params}) do
+    IO.inspect(event_params)
     case Events.create_event(event_params) do
       {:ok, event} ->
         conn
